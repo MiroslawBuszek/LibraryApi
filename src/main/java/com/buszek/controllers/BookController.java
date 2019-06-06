65c75bba01698b0db3591df33423b62e47fa8d21
Mirosław Buszek <zoowett@gmail.com>
2019-06-06 23:21:12 +0200

    Create book controller

--- src/main/java/com/buszek/controllers/BookController.java
+++ src/main/java/com/buszek/controllers/BookController.java
@@ -1,7 +1,32 @@
 package com.buszek.controllers;
 
+import com.buszek.models.Book;
+import com.buszek.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class BookController {
 
+    private BookService booksService;
 
+    @Autowired
+    public BookController(BookService booksService) {
+        this.booksService = booksService;
+    }
+
+    @GetMapping("/books")
+    public List<Book> getAllBooks() {
+        return booksService.findAll();
+    }
+
+    @GetMapping("/books/{id}")
+    public Book getBook(@PathVariable long id) {
+        return booksService.findById(id);
+    }
 
 }
