ff38b1ed5935a1ecc3e8ec0d7591628b9fa77ed8
Mirosław Buszek <zoowett@gmail.com>
2019-06-09 22:21:39 +0200

    Create user model

---
+++ src/main/java/com/buszek/models/User.java
@@ -0,0 +1,34 @@
+package com.buszek.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+
+    Long id;
+    String name;
+    String surname;
+    String nick;
+    String password;
+    String email;
+
+    public void updateUser (User other) {
+
+        if (other != null) {
+
+            if (other.getName() != null) {
+                name = other.getName();
+            } else if (other.getSurname() != null) {
+                surname = other.getSurname();
+            } else if (other.getNick() != null) {
+                nick = other.getNick();
+            } else if (other.getPassword() != null) {
+                password = other.getPassword();
+            } else if (other.getEmail() != null) {
+                email = other.getEmail();
+            }
+        }
+    }
+}
